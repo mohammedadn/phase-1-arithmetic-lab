fbe24236c7f1bcb6a923757fe9b74464c8f925c5
Mohammedadn <mohammedaden023@gmail.com>
2024-03-19 09:05:47 +0300

    arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = num1*num2;
 
-// Write your code here
+const random=Math.floor(Math.random()*1)+1;
+console.log(random);
+
+const num3 = 11;
+const num4 = 7;
+let mod = num3%num4;
+
+const max=Math.max(1,2,20);
+console.log(max);
